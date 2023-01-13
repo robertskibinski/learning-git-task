0ad90407a562a09a3b3d7789dd461ab7d88b0b80
rober <robert.skibinski@poczta.onet.pl>
2023-01-13 20:02:24 +0100

    Cosmetic change

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,6 +1,7 @@
 purchase_list ={'piekarnia':['chleb','bułki','pączek'], 'warzywniak':['Marchew', 'Seler', 'Rukola'], 'Rossmann':['szampon','mydło']}
 
 quantity_of_products=0
+
 for place, products in purchase_list.items():
     products =[product.capitalize() for product in products]
     print(f'Idę do {place.capitalize()} i kupuję tam {products}')
