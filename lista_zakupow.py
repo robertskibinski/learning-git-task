8f0e6a6d441ebe6c41f6ee7fe9e9f7c0a82fef0b
rober <robert.skibinski@poczta.onet.pl>
2023-01-13 19:50:20 +0100

    Added shopping list

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,1 +1,8 @@
-purlist ={}
+purchase_list ={'piekarnia':['chleb','bułki','pączek'], 'warzywniak':['Marchew', 'Seler', 'Rukola']}
+
+quantity_of_products=0
+for place, products in purchase_list.items():
+    products =[product.capitalize() for product in products]
+    print(f'Idę do {place.capitalize()} i kupuję tam {products}')
+    quantity_of_products += len(products)
+print(f'Kupuję w sumie {quantity_of_products} produktów')
